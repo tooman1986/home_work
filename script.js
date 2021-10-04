d2af3bcee63acfb649b69152785e4cfca576737e
tooman86 <viktormishnev86@mail.ru>
2021-10-04 09:02:48 +0300

    home_work

---
+++ script.js
@@ -0,0 +1,37 @@
+let one = 1;
+let two = 2;
+
+console.log(one, two);
+
+
+[one, two] = [two, one];
+console.log(one, two);
+
+const numbers = [10, 11, 12, 13, 14, 15, 16, 17, 18, 19];
+let amount = numbers [2] + numbers [3] + numbers [4];
+console.log(amount);
+
+
+
+function func() {
+    let one = 1;
+    let two = 2;
+
+console.log(one, two);
+
+[one, two] = [two, one];
+console.log(one, two);
+
+const numbers = [10, 11, 12, 13, 14, 15, 16, 17, 18, 19];
+let amount = numbers [2] + numbers [3] + numbers [4];
+console.log(amount);
+
+let all = {
+    one: 1,
+    two: 2,
+    sum: amount,
+};
+console.log(all);
+}
+
+func();
