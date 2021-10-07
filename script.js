1bbd8ba04b072723dc95f8bc9a875ceea8938c54
tooman86 <viktormishnev86@mail.ru>
2021-10-07 07:44:26 +0300

    homework2

--- script.js
+++ script.js
@@ -1,37 +1,40 @@
-let one = 1;
-let two = 2;
+// task 1
 
-console.log(one, two);
+let i = 0;
 
-
-[one, two] = [two, one];
-console.log(one, two);
-
-const numbers = [10, 11, 12, 13, 14, 15, 16, 17, 18, 19];
-let amount = numbers [2] + numbers [3] + numbers [4];
-console.log(amount);
-
-
-
-function func() {
-    let one = 1;
-    let two = 2;
-
-console.log(one, two);
-
-[one, two] = [two, one];
-console.log(one, two);
-
-const numbers = [10, 11, 12, 13, 14, 15, 16, 17, 18, 19];
-let amount = numbers [2] + numbers [3] + numbers [4];
-console.log(amount);
-
-let all = {
-    one: 1,
-    two: 2,
-    sum: amount,
-};
-console.log(all);
+while( i <= 49 ) {
+    i++;
+    console.log(i);
 }
 
-func();
+// task 2
+
+for (let even = 2; even <= 100; even++) {
+    if (even % 2 == 0) {
+      console.log(even);
+    }
+  }
+
+  //task 3
+
+  let arr = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20];
+  function arraySum(array){
+  let sum = 0;
+  for(let i = 0; i < array.length; i++){
+      sum += array[i];
+      }
+  alert(sum);
+  }
+  arraySum(arr);
+
+
+
+//задание с занятия - числа кратные 5
+
+
+  for (let item = 5; item <= 1000; item++) {
+    if (item % 5 == 0) {
+      console.log(item);
+    }
+  }
+
