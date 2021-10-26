67143b9bda906f87f5a097f7ffd5e912e3554238
tooman86 <viktormishnev86@mail.ru>
2021-10-26 13:59:28 +0300

    home_work_forms

--- script.js
+++ script.js
@@ -1,36 +1,53 @@
 'use strict';
 
-///// Task 1
+///// Task 3  Создать форму в HTML с полем ввода, несколькими checkbox и кнопкой отправки. На submit формы вывести значения поля ввода и имена всех отмеченных полей checkbox.
 
-let liEl = document.querySelectorAll('li');
-let ulEl = document.querySelector('ul');
+let form = document.forms['form'];
+let tag = form.elements;
 
-for (let i = 0; i < liEl.length; i++) {
-    liEl[i].addEventListener('mouseenter', () => {
-        ulEl.style.display = 'none';
-    });
-}
+ form.addEventListener('submit', function (event) {
+     event.preventDefault();
+     console.log(tag.name.value);
+     for(let i = 0; i < tag.length; i++) {
+         if(tag[i].type == 'checkbox' && tag[i].checked) {
+             console.log(tag[i].name);
+         }
+     }
+ });
 
-//// Task 2
 
-const btnAdd = document.getElementById("btnAdd");
-const btnDel = document.getElementById("btnDel");
+//// Task 4 Создать форму в HTML с полем ввода и кнопкой отправки. На submit формы, если поля ввода пустое, делать фокус на поле ввода.
+ 
+let form1 = document.forms['form1'];
+let tag1 = form1.elements;
+
+ form1.addEventListener('submit', function (event) {
+     if(tag1.name.value == '') {
+      tag1.name.focus();
+     }
+ });
 
 
-btnAdd.addEventListener('click', () => {
-    let newEl = document.createElement('p');
-    document.body.append(newEl);
-    newEl.innerHTML = 'text text text';
-    
+ ///// Task 5 Создать форму в HTML с полями для ввода имени, фамилии, телефона и сообщения. Для поля телефона задать максимальное количество символов 13, сообщение может быть большим. При submit формы создавать объект со всеми значениями формы и выводить его.
+
+let form2 = document.forms['form2'];
+let tag2 = form2.elements;
+
+form2.addEventListener('submit', function (event) {
+    event.preventDefault();
+
+    let name = tag2.name.value;
+    let surname = tag2.surname.value;
+    let phone = tag2.phone.value;
+    let message = tag2.message.value;
+
+    const obj = {
+        имя: name,
+        фамилия: surname,
+        телефон: phone,
+        сообщение: message,
+        };
+
+console.log(obj);
+
 });
-
-btnDel.addEventListener('click', () => {
-    let arr = [];
-    let elOfArr = document.querySelectorAll('p');
-    elOfArr.forEach(function (x) {
-        arr.push(x);
-    });
-    arr.pop().remove();
-});
-
-
